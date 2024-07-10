7478657f9781ecafaca765b6d028b8bb424d8c84
muqeeth26832 <mdabdulmuqeeth26832@gmail.com>
2024-07-10 16:11:42 +0530

    2 - UI design completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          ...daisyUIThemes["black"],
+          primary: "rgb(29, 155, 240)",
+          secondary: "rgb(24, 24, 24)",
+        },
+      },
+    ],
+  },
+};
