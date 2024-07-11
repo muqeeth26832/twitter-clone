9dca8a18d8c561f7c46a2d5e32e3bf88c4da9d57
muqeeth26832 <mdabdulmuqeeth26832@gmail.com>
2024-07-11 17:40:04 +0530

    3 - suggested users function added

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,10 +1,30 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const RightPanel = () => {
-  const isLoading = false;
+  const { data: suggestedUsers, isLoading } = useQuery({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/users/suggested");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message, "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  if(suggestedUsers?.length ==0 ){
+    <div className="md:w-64 sm:w-0">
+
+    </div>
+  }
 
   return (
     <div className="hidden lg:block my-4 mx-2">
@@ -21,7 +41,7 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            USERS_FOR_RIGHT_PANEL?.map((user) => (
+            suggestedUsers?.map((user) => (
               <Link
                 to={`/profile/${user.username}`}
                 className="flex items-center justify-between gap-4"
