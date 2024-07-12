1303537326506152faf0c46c0d64842fa907907c
muqeeth26832 <mdabdulmuqeeth26832@gmail.com>
2024-07-12 18:40:16 +0530

    4  - prepared App for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -19,6 +20,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json({ limit: "5mb" }));
@@ -29,6 +31,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notifificationsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(8000, () => {
   console.log(`Server is running on port ${PORT}`);
